42eddda21385ceebf70ad8199aa22516e0c44b70
Evan Lewis <lewisev@udel.edu>
2023-03-20 17:30:29 -0400

    solved forms

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,51 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const [username, setUsername] = useState<string>("Your Name");
+    const [isStudent, setStudent] = useState<boolean>(true);
+
+    const editing = () => {
+        return (
+            <div>
+                <Form.Group>
+                    <Form.Label>Name:</Form.Label>
+                    <Form.Control
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <Form.Check
+                        type="checkbox"
+                        id="studentCheckbox"
+                        label="Student?"
+                        checked={isStudent}
+                        onChange={() => setStudent(!isStudent)}
+                    />
+                </Form.Group>
+            </div>
+        );
+    };
+
+    const viewing = () => {
+        return isStudent ? (
+            <p>{username} is a student</p>
+        ) : (
+            <p>{username} is not a student</p>
+        );
+    };
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            {editMode ? editing() : viewing()}
+            <Form.Check
+                type="switch"
+                id="editmodeSwitch"
+                label="Edit Mode"
+                checked={editMode}
+                onChange={() => setEditMode(!editMode)}
+            />
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,33 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attempts, setAttempts] = useState<number>(3);
+    const [requested, setRequested] = useState<string>("0");
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <Form.Group controlId="attemptsForm">
+                <Form.Control
+                    type="number"
+                    onChange={(e) => setRequested(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+            <Button
+                onClick={() => setAttempts(attempts - 1)}
+                disabled={attempts <= 0}
+            >
+                Use attempt
+            </Button>
+            <Button
+                onClick={() => {
+                    setAttempts(attempts + (parseInt(requested) || 0));
+                }}
+            >
+                Gain
+            </Button>
+            <p>Number of Attempts: {attempts}</p>
         </div>
     );
 }
